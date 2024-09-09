f2088cafff3a4297a591f551b9569165e2302727
Natalia Cristina <nataliacristina586@gmail.com>
2024-09-09 19:15:38 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [{
+    titulo: "Grand Theft Auto V",
+    descricao: "É um jogo de mundo aberto que coloca os jogadores no coração de Los Santos, uma cidade fictícia inspirada em Los Angeles. A trama envolvente acompanha três protagonistas: um ex-ladrão de bancos, um psicopata e um jovem delinquente, que se veem envolvidos em uma série de crimes e missões perigosas.",
+    tags: "los santos"
+},
+
+{
+    titulo: "Red Dead Redemption 2",
+    descricao: "É uma imersão épica no Velho Oeste, pouco antes do início da era moderna. Acompanhamos Arthur Morgan, um fora da lei que faz parte da gangue de Van der Linde, em uma jornada repleta de desafios e dilemas morais. O jogo se destaca por seus gráficos impressionantes, mundo aberto detalhado e história envolvente, que explora temas como lealdade, amizade e a inevitabilidade do progresso. Ao explorar o vasto cenário americano, os jogadores podem caçar, pescar, interagir com outros personagens e vivenciar uma experiência autêntica do Velho Oeste.",
+    tags: "velho oeste"
+},
+
+{
+    titulo: "Resident Evil 4",
+    descricao: "É um jogo de terror de sobrevivência onde você controla Leon S. Kennedy em uma missão para resgatar a filha do presidente dos Estados Unidos, sequestrada por um culto misterioso em uma vila europeia. O jogo combina ação, exploração e quebra-cabeças, enquanto você enfrenta hordas de inimigos infectados e criaturas grotescas.",
+    tags: "leon"
+},
+
+{
+    titulo: "Fortnite",
+    descricao: "É um jogo online gratuito onde você e outros jogadores lutam para serem os últimos sobreviventes em uma ilha, construindo estruturas para se proteger e eliminando os adversários.",
+    tags: "lutam"
+},
+
+{
+    titulo: "DayZ",
+    descricao: "É um jogo de sobrevivência em mundo aberto onde você é jogado em um mundo pós-apocalíptico infestado de zumbis. A sua principal missão é sobreviver, encontrando comida, água, abrigo e armas para se defender dos zumbis e de outros jogadores que podem ser tanto aliados quanto inimigos.",
+    tags: "zumbis"
+},
+
+{
+    titulo: "Ghost Recon Wildlands",
+    descricao: "É um jogo de tiro tático em mundo aberto ambientado na Bolívia. Você lidera um esquadrão de elite (os Ghosts) com a missão de desmantelar um poderoso cartel de drogas. O jogo oferece liberdade para explorar o mapa vasto e realizar missões de diversas formas, utilizando táticas furtivas ou força bruta.",
+    tags: "tatico"
+}
+
+
+]
